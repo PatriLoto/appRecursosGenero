09a64e83ce39686dbe2e8ba81a18468f2f2d88b2
Patricia Loto <patricialoto@hotmail.com>
2019-10-28 15:35:27 -0300

    mejoras a implementar

---
+++ recursosMejorasAImplementar.R
@@ -0,0 +1,17 @@
+# links para mejorar la app:
+#tutorial: https://rpubs.com/moviedo/shiny
+
+#http://shiny.rstudio.com/gallery/widgets.html
+
+#1- seleccionar por tipo de recurso
+#   http://shiny.rstudio.com/gallery/widgets.html
+
+#2- Descargar archivo
+#   http://shiny.rstudio.com/gallery/file-download.html
+
+#3- Búsqueda por recurso
+#   http://shiny.rstudio.com/gallery/basic-datatable.html
+
+#4- nube de palabras
+#   http://shiny.rstudio.com/gallery/word-cloud.html
+
